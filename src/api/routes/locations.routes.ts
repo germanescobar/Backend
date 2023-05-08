4ab715655274745d3808ab82d3b4384e1976bd3a
JeanVittory <vittory.dev@gmail.com>
2023-05-07 20:55:52 -0500

    fix(tsconfig): type:module

--- src/api/routes/locations.routes.ts
+++ src/api/routes/locations.routes.ts
@@ -1,7 +1,5 @@
 import { Router } from 'express';
-//import LocationsController from '../controllers/Locations.controller';
-const LocationsController = require('../controllers/Locations.controller');
-
+import LocationsController from '../controllers/Locations.controller';
 const locationRouter = Router();
 
 locationRouter.get('/', LocationsController.getLocations);
