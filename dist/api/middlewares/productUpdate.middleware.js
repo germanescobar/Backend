476d8aea813cb617c39db6801ede189ddc809282
JeanVittory <vittory.dev@gmail.com>
2023-05-07 20:36:17 -0500

    fix(tsconfig): deleting dist

--- dist/api/middlewares/productUpdate.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const productUpdateValidator = (req, res, next) => {
-    const { price, stock, discount } = req.body;
-    req.body = Object.assign(Object.assign({}, req.body), { price: Number(parseFloat(price) * 100), stock: +stock, discount: +discount });
-    next();
-};
-exports.default = productUpdateValidator;
--- dist/api/routes/categories.routes.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const Categories_controller_1 = require("../controllers/Categories.controller");
-const categoriesRouter = (0, express_1.Router)();
-categoriesRouter.get('/', Categories_controller_1.CategoriesController.getCategories);
-exports.default = categoriesRouter;
